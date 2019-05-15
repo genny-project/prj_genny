31a9d2fb670e64606b85cd1a051e613f05bb2f01
Adam Crow <adamcrow63@gmail.com>
2019-05-15 22:51:50 +1000

    Working but still no QRules read

--- src/test/java/life/genny/test/JavaServiceQuickstartTest.java
+++ src/test/java/life/genny/test/JavaServiceQuickstartTest.java
@@ -53,17 +53,26 @@ import org.kie.internal.command.CommandFactory;
 import org.kie.internal.persistence.jpa.JPAKnowledgeService;
 import org.kie.internal.runtime.StatefulKnowledgeSession;
 
+import io.vavr.Tuple2;
 import io.vavr.Tuple3;
+import life.genny.eventbus.EventBusInterface;
+import life.genny.eventbus.EventBusMock;
+import life.genny.eventbus.MockCache;
+import life.genny.eventbus.VertxCache;
 import life.genny.qwanda.entity.Person;
 import life.genny.qwanda.message.QEventMessage;
+import life.genny.qwandautils.GennyCacheInterface;
 import life.genny.qwandautils.GennySettings;
+import life.genny.qwandautils.KeycloakUtils;
+import life.genny.qwandautils.SecurityUtils;
+import life.genny.rules.QRules;
 import life.genny.rules.RulesLoader;
+import life.genny.utils.VertxUtils;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.kie.api.command.Command;
 import org.kie.api.runtime.KieSession;
-
 
 import static org.junit.Assert.*;
 
@@ -75,15 +84,14 @@ public class JavaServiceQuickstartTest extends JbpmJUnitBaseTestCase {
 			.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
 
 	protected final org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
-    
+
 	protected static EntityManagerFactory emf;
 	protected static EntityManager em;
-	
+
 	protected static String realm = GennySettings.mainrealm;
 	protected static Set<String> realms;
-	
-    private static final String CONDITIONAL_ID = "org.jbpm.test.functional.event.StartEvent-conditional";
 
+	private static final String CONDITIONAL_ID = "org.jbpm.test.functional.event.StartEvent-conditional";
 
 	@BeforeClass
 	public static void init() throws FileNotFoundException, SQLException {
@@ -92,8 +100,9 @@ public class JavaServiceQuickstartTest extends JbpmJUnitBaseTestCase {
 			emf = Persistence.createEntityManagerFactory("h2-pu");
 			env = EnvironmentFactory.newEnvironment(); // KnowledgeBaseFactory.newEnvironment();
 			env.set(EnvironmentName.ENTITY_MANAGER_FACTORY, emf);
-	//		   env.set(EnvironmentName.TRANSACTION_MANAGER, TransactionManagerServices.getTransactionManager());
-			    env.set(EnvironmentName.GLOBALS, new MapGlobalResolver());
+			// env.set(EnvironmentName.TRANSACTION_MANAGER,
+			// TransactionManagerServices.getTransactionManager());
+			env.set(EnvironmentName.GLOBALS, new MapGlobalResolver());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -103,15 +112,15 @@ public class JavaServiceQuickstartTest extends JbpmJUnitBaseTestCase {
 			System.out.println("Setting up EntityManager");
 			em = emf.createEntityManager();
 		}
-		
-		//Set up realm
+
+		// Set up realm
 		realms = new HashSet<String>();
 		realms.add(realm);
 		realms.stream().forEach(System.out::println);
 		realms.remove("genny");
 
-		 //Enable the PseudoClock using the following system property.
-	    System.setProperty("drools.clockType", "pseudo");
+		// Enable the PseudoClock using the following system property.
+		System.setProperty("drools.clockType", "pseudo");
 	}
 
 	static Environment env; // drools persistence
@@ -125,44 +134,28 @@ public class JavaServiceQuickstartTest extends JbpmJUnitBaseTestCase {
 	@Test(timeout = 30000)
 	public void testProcess() {
 
-		  KieSession kieSession = createKSession("rulesCurrent/shared/00_Startup/auth_init.bpmn");
-//		  KieSession kieSession = createKSession("rulesCurrent/shared/00_Startup/test.bpmn");
+		KieSession kieSession = createKSession("rulesCurrent/shared/00_Startup/auth_init.bpmn");
 //		KieSession kieSession = setupSession("/rulesCurrent/shared/00_Startup",true);
-		
-		
-//		Map<String, Object> params = new HashMap<String, Object>();
+
 //
 		QEventMessage msg = new QEventMessage("EVT_MSG", "AUTH_INIT");
-//		params.put("msg", msg);
-//		params.put("inout", "condition");
-//		kieSession.insert(msg);
-//		kieSession.insert(new String("condition"));
-		
-	       List<Command<?>> cmds = new ArrayList<Command<?>>();
-	        cmds.add(getCommands().newInsert(msg));
-	//        cmds.add(getCommands().newInsert("condition"));
-	//        kieSession.execute(getCommands().newBatchExecution(cmds, null));
-	        
-//	        BatchExecutionCommand command = new BatchExecutionCommand();
-//
-//	        command.setLookup("ksession1");
-//
-//	        StartProcessCommand startProcessCommand = new StartProcessCommand();
-//
-//	        startProcessCommand.setProcessId("org.drools.task.processOne");
-//
-//	        command.getCommands().add(startProcessCommand);
-//	        
-	 //       cmds.add( CommandFactory.newSetGlobal( "list1", new ArrayList(), true ) );
-	        cmds.add( CommandFactory.newInsert( msg, "msg" ) );
-	    //    cmds.add( CommandFactory.newQuery( "Get Msg" "getMsg" );
 
-	        ExecutionResults results = kieSession.execute( CommandFactory.newBatchExecution( cmds ) );
-	 //       results.getValue( "list1" ); // returns the ArrayList
-	        results.getValue( "msg" ); // returns the inserted fact Person
-	  //      results.getValue( "Get People" );// returns the query as a QueryResults instance.
-	        System.out.println(results.getValue("msg"));
+		List<Command<?>> cmds = new ArrayList<Command<?>>();
+		cmds.add(getCommands().newInsert(msg));
+		QRules qRules = getQRules(realm, "user1","Barry Allan", "hero"); // defaults to user anyway
+		cmds.add(CommandFactory.newInsert(qRules, "rules"));
+		cmds.add(CommandFactory.newInsert(msg, "msg"));
+		cmds.add(CommandFactory.newInsert("GADA", "name"));
+		// cmds.add( CommandFactory.newQuery( "Get Msg" "getMsg" );
 
+		ExecutionResults results = kieSession.execute(CommandFactory.newBatchExecution(cmds));
+		// results.getValue( "list1" ); // returns the ArrayList
+		results.getValue("msg"); // returns the inserted fact Msg
+		results.getValue("rules"); // returns the inserted fact QRules
+		// results.getValue( "Get People" );// returns the query as a QueryResults
+		// instance.
+		System.out.println(results.getValue("msg"));
+		System.out.println(results.getValue("rules"));
 //	        SignalEventCommand signalEventCommand = new SignalEventCommand();
 //
 //	        signalEventCommand.setProcessInstanceId(1001);
@@ -172,106 +165,93 @@ public class JavaServiceQuickstartTest extends JbpmJUnitBaseTestCase {
 //	        signalEventCommand.setEvent(msg);
 //
 //	        command.getCommands().add(signalEventCommand);
-	        
 
 //		 ProcessInstance pid = kieSession.startProcess("process_1", params);
 //		 System.out.println("Process Instance id="+pid.getId());
-
-		
-
 
 		// recreate the session from database using the sessionId
 
 		// ksession = JPAKnowledgeService.loadStatefulKnowledgeSession( sessionId,
 		// kbase, null, env );
-		 
-	      // check whether the process instance has completed successfully
+
+		// check whether the process instance has completed successfully
 //	      assertProcessInstanceCompleted(pid.getId(), kieSession);
-	      // check whether the given nodes were executed during the process execution
-	      //assertNodeTriggered(pid.getId(), "StartProcess", "Hello", "EndProcess");
+		// check whether the given nodes were executed during the process execution
+		// assertNodeTriggered(pid.getId(), "StartProcess", "Hello", "EndProcess");
 
-	      
-
-
-	      
-	      
-		 kieSession.dispose();
+		kieSession.dispose();
 
 	}
-	
-	//@Test
-	public void basicTest()
-	{
+
+	// @Test
+	public void basicTest() {
 		RuleFlowProcessFactory factory =
 
-			    RuleFlowProcessFactory.createProcess("org.jbpm.HelloWorld");
+				RuleFlowProcessFactory.createProcess("org.jbpm.HelloWorld");
 
-			factory
+		factory
 
-			    // Header
+				// Header
 
-			    .name("HelloWorldProcess")
+				.name("HelloWorldProcess")
 
-			    .version("1.0")
+				.version("1.0")
 
-			    .packageName("org.jbpm")
+				.packageName("org.jbpm")
 
-			    // Nodes
+				// Nodes
 
-			    .startNode(1).name("Start").done()
+				.startNode(1).name("Start").done()
 
-			    .actionNode(2).name("Action")
+				.actionNode(2).name("Action")
 
-			        .action("java", "System.out.println(\"Hello World\");").done()
+				.action("java", "System.out.println(\"Hello World\");").done()
 
-			    .endNode(3).name("End").done()
+				.endNode(3).name("End").done()
 
-			    // Connections
+				// Connections
 
-			    .connection(1, 2)
+				.connection(1, 2)
 
-			    .connection(2, 3);
+				.connection(2, 3);
 
-			RuleFlowProcess process = factory.validate().getProcess();
-			
+		RuleFlowProcess process = factory.validate().getProcess();
+
 	}
-	
-	//@Test(timeout = 30000)
+
+	// @Test(timeout = 30000)
 	public void testTimerProcess() {
 
-		  KieSession kieSession = createKSession("rulesCurrent/shared/00_Startup/process_2.bpmn");
-	//	  KieSession kieSession = setupSession("/rulesCurrent/shared/00_Startup/process_2.bpmn",true);
-		
-	       List<Command<?>> cmds = new ArrayList<Command<?>>();
+		KieSession kieSession = createKSession("rulesCurrent/shared/00_Startup/process_2.bpmn");
+		// KieSession kieSession =
+		// setupSession("/rulesCurrent/shared/00_Startup/process_2.bpmn",true);
 
-	       ProcessInstance pid = kieSession.startProcess("process_2", null);
-	   //     ExecutionResults results = kieSession.execute( CommandFactory.newBatchExecution( cmds ) );
+		List<Command<?>> cmds = new ArrayList<Command<?>>();
 
-	        try {
-				Thread.sleep(15000);
-			} catch (InterruptedException e) {
+		ProcessInstance pid = kieSession.startProcess("process_2", null);
+		// ExecutionResults results = kieSession.execute(
+		// CommandFactory.newBatchExecution( cmds ) );
 
-			}
+		try {
+			Thread.sleep(15000);
+		} catch (InterruptedException e) {
 
-	      
-	      
-		 kieSession.dispose();
+		}
+
+		kieSession.dispose();
 
 	}
-	
-	
-	private KieSession setupSession(final String rulesDir, final boolean debug)
-	{
+
+	private KieSession setupSession(final String rulesDir, final boolean debug) {
 		String baseRulesDir = "./rules"; // default for project
 		if (!"/rules".equals(GennySettings.rulesDir)) {
 			baseRulesDir = GennySettings.rulesDir;
 		}
-		String testRulesDir = baseRulesDir+rulesDir;
+		String testRulesDir = baseRulesDir + rulesDir;
 
-		List<Tuple3<String, String, String>> rules = RulesLoader.processFileRealms("genny", testRulesDir,
-				realms);
+		List<Tuple3<String, String, String>> rules = RulesLoader.processFileRealms("genny", testRulesDir, realms);
 		Integer rulesCount = RulesLoader.setupKieRules(realm, rules);
-		System.out.println("Rules Count for " + realm +":"+testRulesDir+ " = " + rulesCount);
+		System.out.println("Rules Count for " + realm + ":" + testRulesDir + " = " + rulesCount);
 
 		StatefulKnowledgeSession kieSession = null;
 		KieSessionConfiguration ksconf = KieServices.Factory.get().newKieSessionConfiguration();
@@ -281,19 +261,17 @@ public class JavaServiceQuickstartTest extends JbpmJUnitBaseTestCase {
 		KieBase kieBase = RulesLoader.getKieBaseCache().get(realm);
 		kieSession = (StatefulKnowledgeSession) kieBase.newKieSession(ksconf, env);
 
-
 		int sessionId = kieSession.getId();
 		System.out.println("Session id = " + sessionId);
 
 		if (debug) {
 			setDebug(kieSession);
 		}
-		
+
 		return kieSession;
 
 	}
-	
-	
+
 	@Test
 	public void testTimerActivated() {
 //	    ProcessInstance pInstance = kieSession.startProcess("sample-process");
@@ -307,7 +285,7 @@ public class JavaServiceQuickstartTest extends JbpmJUnitBaseTestCase {
 //	    assertNodeTriggered(pInstanceId, "Goodbye Process");
 //	    assertProcessInstanceCompleted(pInstanceId);
 	}
-	
+
 	private void setDebug(KieSession kieSession) {
 		kieSession.addEventListener(new DefaultProcessEventListener() {
 			public void beforeProcessStarted(ProcessStartedEvent event) {
@@ -356,33 +334,33 @@ public class JavaServiceQuickstartTest extends JbpmJUnitBaseTestCase {
 			public void afterNodeTriggered(ProcessNodeTriggeredEvent event) {
 				WorkflowProcessInstance process = (WorkflowProcessInstance) event.getProcessInstance();
 				NodeInstance node = event.getNodeInstance();
-				System.out.println("jBPM event 'afterNodeTriggered'. Process ID: " + process.getId() + ", Process definition ID: "
-						+ process.getProcessId() + ", Process name: " + process.getProcessName() + ", Process state: "
-						+ process.getState() + ", Parent process ID: " + process.getParentProcessInstanceId()
-						+ ", Node instance ID: " + node.getId() + ", Node ID: " + node.getNodeId() + ", Node name: "
-						+ node.getNodeName());
+				System.out.println("jBPM event 'afterNodeTriggered'. Process ID: " + process.getId()
+						+ ", Process definition ID: " + process.getProcessId() + ", Process name: "
+						+ process.getProcessName() + ", Process state: " + process.getState() + ", Parent process ID: "
+						+ process.getParentProcessInstanceId() + ", Node instance ID: " + node.getId() + ", Node ID: "
+						+ node.getNodeId() + ", Node name: " + node.getNodeName());
 
 			}
 
 			public void beforeNodeLeft(ProcessNodeLeftEvent event) {
 				WorkflowProcessInstance process = (WorkflowProcessInstance) event.getProcessInstance();
 				NodeInstance node = event.getNodeInstance();
-				System.out.println("jBPM event 'beforeNodeLeft'. Process ID: " + process.getId() + ", Process definition ID: "
-						+ process.getProcessId() + ", Process name: " + process.getProcessName() + ", Process state: "
-						+ process.getState() + ", Parent process ID: " + process.getParentProcessInstanceId()
-						+ ", Node instance ID: " + node.getId() + ", Node ID: " + node.getNodeId() + ", Node name: "
-						+ node.getNodeName());
+				System.out.println("jBPM event 'beforeNodeLeft'. Process ID: " + process.getId()
+						+ ", Process definition ID: " + process.getProcessId() + ", Process name: "
+						+ process.getProcessName() + ", Process state: " + process.getState() + ", Parent process ID: "
+						+ process.getParentProcessInstanceId() + ", Node instance ID: " + node.getId() + ", Node ID: "
+						+ node.getNodeId() + ", Node name: " + node.getNodeName());
 
 			}
 
 			public void afterNodeLeft(ProcessNodeLeftEvent event) {
 				WorkflowProcessInstance process = (WorkflowProcessInstance) event.getProcessInstance();
 				NodeInstance node = event.getNodeInstance();
-				System.out.println("jBPM event 'afterNodeLeft'. Process ID: " + process.getId() + ", Process definition ID: "
-						+ process.getProcessId() + ", Process name: " + process.getProcessName() + ", Process state: "
-						+ process.getState() + ", Parent process ID: " + process.getParentProcessInstanceId()
-						+ ", Node instance ID: " + node.getId() + ", Node ID: " + node.getNodeId() + ", Node name: "
-						+ node.getNodeName());
+				System.out.println("jBPM event 'afterNodeLeft'. Process ID: " + process.getId()
+						+ ", Process definition ID: " + process.getProcessId() + ", Process name: "
+						+ process.getProcessName() + ", Process state: " + process.getState() + ", Parent process ID: "
+						+ process.getParentProcessInstanceId() + ", Node instance ID: " + node.getId() + ", Node ID: "
+						+ node.getNodeId() + ", Node name: " + node.getNodeName());
 
 			}
 
@@ -410,28 +388,65 @@ public class JavaServiceQuickstartTest extends JbpmJUnitBaseTestCase {
 
 		});
 	}
-	
-	   public KieSession createKSession(String... process) {
-	        createRuntimeManager(process);
-	        return getRuntimeEngine().getKieSession();
-	    }
 
-	    public KieSession createKSession(Map<String, ResourceType> res) {
-	        createRuntimeManager(res);
-	        return getRuntimeEngine().getKieSession();
-	    }
+	public KieSession createKSession(String... process) {
+		createRuntimeManager(process);
+		return getRuntimeEngine().getKieSession();
+	}
 
-	    public KieSession restoreKSession(String... process) {
-	        disposeRuntimeManager();
-	        createRuntimeManager(process);
-	        return getRuntimeEngine().getKieSession();
-	    }
-	  protected static KieServices getServices() {
-	        return KieServices.Factory.get();
-	    }
+	public KieSession createKSession(Map<String, ResourceType> res) {
+		createRuntimeManager(res);
+		return getRuntimeEngine().getKieSession();
+	}
 
-	    protected static KieCommands getCommands() {
-	        return getServices().getCommands();
-	    }
+	public KieSession restoreKSession(String... process) {
+		disposeRuntimeManager();
+		createRuntimeManager(process);
+		return getRuntimeEngine().getKieSession();
+	}
+
+	protected static KieServices getServices() {
+		return KieServices.Factory.get();
+	}
+
+	protected static KieCommands getCommands() {
+		return getServices().getCommands();
+	}
+
+	private QRules getQRules(final String realm, final String username, final String name, final String role) {
+
+		List<Tuple2<String, Object>> globals = new ArrayList<Tuple2<String, Object>>();
+		Map<String, String> keyValueMap = new HashMap<String, String>();
+
+		globals = RulesLoader.getStandardGlobals();
+
+		Map<String, Object> adecodedTokenMap = new HashMap<String, Object>();
+		adecodedTokenMap.put("preferred_username",username);
+		adecodedTokenMap.put("name", name);
+		adecodedTokenMap.put("realm", realm);
+		adecodedTokenMap.put("realm_access", "[user," + role + "]");
+
+		Set<String> auserRoles = KeycloakUtils.getRoleSet(adecodedTokenMap.get("realm_access").toString());
+
+		String realm2 = adecodedTokenMap.get("realm").toString();
+		if ("genny".equalsIgnoreCase(realm2)) {
+			realm2 = GennySettings.mainrealm;
+			adecodedTokenMap.put("realm", GennySettings.mainrealm);
+		}
+		String jwtToken = null;
+
+		jwtToken = SecurityUtils.createJwt("ABBCD", "Genny Project", "Test JWT", 100000, "IamASecret",
+				adecodedTokenMap);
+
+		EventBusInterface eventBusMock = new EventBusMock();
+		GennyCacheInterface vertxCache = new MockCache();
+		VertxUtils.init(eventBusMock, vertxCache);
+
+		QRules qRules = new QRules(eventBusMock, jwtToken, adecodedTokenMap);
+		qRules.set("realm", realm);
+
+		return qRules;
+
+	}
 
 }
