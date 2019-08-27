aae8a7acf2ae4eb42a98d2ded97abd1b69613eec
Chris Pyke <ChrisPyke@Domenics-MacBook-Pro.local>
2019-08-27 14:24:33 +1000

    Workflows

--- src/test/java/life/genny/test/ChrisTest.java
+++ src/test/java/life/genny/test/ChrisTest.java
@@ -1,30 +1,53 @@
 package life.genny.test;
 
+import org.kie.api.runtime.process.ProcessContext;
 import java.io.FileNotFoundException;
 import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Type;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.Date; // Added by Chris
 
 import org.apache.logging.log4j.Logger;
 import org.codehaus.plexus.util.StringUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.google.gson.reflect.TypeToken;
+
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vertx.core.json.JsonObject;
 import life.genny.eventbus.EventBusInterface;
 import life.genny.eventbus.EventBusMock;
 import life.genny.eventbus.VertxCache;
+import life.genny.models.Frame3;
+import life.genny.models.FramePosition;
 import life.genny.models.GennyToken;
+import life.genny.models.Theme;
+import life.genny.models.ThemeAttributeType;
+import life.genny.models.ThemeDouble;
 import life.genny.qwanda.Answer;
+import life.genny.qwanda.Context;
+import life.genny.qwanda.ContextType;
+import life.genny.qwanda.VisualControlType;
 import life.genny.qwanda.entity.BaseEntity;
 import life.genny.qwanda.message.QDataAnswerMessage;
+import life.genny.qwanda.message.QDataAskMessage;
+import life.genny.qwanda.message.QDataBaseEntityMessage;
 import life.genny.qwanda.message.QEventMessage;
 import life.genny.qwandautils.GennyCacheInterface;
 import life.genny.qwandautils.GennySettings;
 import life.genny.qwandautils.JsonUtils;
+import life.genny.qwandautils.QwandaUtils;
 import life.genny.rules.QRules;
+import life.genny.utils.BaseEntityUtils;
+import life.genny.utils.FrameUtils2;
 import life.genny.utils.VertxUtils;
 
 public class ChrisTest {
@@ -41,9 +64,10 @@ public class ChrisTest {
 	public ChrisTest() {
 
 	}
-
-	@Test
-	public void newUserTest() {
+	
+	//@Test
+	public void myFirstTest() {
+		
 		System.out.println("New User test");
 		GennyToken userToken = null;
 		GennyToken userToken2 = null;
@@ -63,23 +87,80 @@ public class ChrisTest {
 			userToken = new GennyToken("userToken", qRules.getToken());
 			serviceToken = new GennyToken("PER_SERVICE", qRules.getServiceToken());
 		}
+		
+		GennyKieSession gks = null;
 
-		System.out.println("session     =" + userToken.getSessionCode());
-		System.out.println("userToken   =" + userToken.getToken());
-		System.out.println("userToken2   =" + userToken2.getToken());
-		System.out.println("serviceToken=" + serviceToken.getToken());
+		try {
+			gks = GennyKieSession.builder(serviceToken, true)
+					//.addDrl("SignalProcessing")
+					//.addDrl("DataProcessing")
+					//.addDrl("EventProcessing")
+					//.addDrl("InitialiseProject")
+					//.addJbpm("InitialiseProject")
+					//.addJbpm("Lifecycles")
+					//.addDrl("AuthInit")
+					.addJbpm("XXXtimer-process.bpmn")
+//					.addJbpm("userSession.bpmn")
+					.addToken(userToken).build();
+			
+			gks.start();
+			
+			String dude = "chris";
+			gks.injectSignal(dude, userToken);
+			
+			// This advances the session by 20 secs to allow timer time to fire
+			gks.advanceSeconds(20);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			if (gks != null) {
+				gks.close();
+			}
+		}
+		
+	}
+
+	@Test
+	public void newUserTest() {
+		System.out.println("New User test");
+		GennyToken userToken = null;
+		GennyToken userToken2 = null;
+		GennyToken serviceToken = null;
+		QRules qRules = null;
+
+		if (true) {
+			userToken = GennyJbpmBaseTest.createGennyToken(realm, "user13", "Barry Allan", "user");
+			userToken2 = GennyJbpmBaseTest.createGennyToken(realm, "user2", "Barry2 Allan2", "user");
+			serviceToken = GennyJbpmBaseTest.createGennyToken(realm, "service", "Service User", "service");
+			qRules = new QRules(eventBusMock, userToken.getToken());
+			qRules.set("realm", userToken.getRealm());
+			qRules.setServiceToken(serviceToken.getToken());
+			VertxUtils.cachedEnabled = true; // don't send to local Service Cache
+		} else {
+			qRules = GennyJbpmBaseTest.setupLocalService();
+			userToken = new GennyToken("userToken", qRules.getToken());
+			serviceToken = new GennyToken("PER_SERVICE", qRules.getServiceToken());
+		}
+
+		System.out.println("session      = " + userToken.getSessionCode());
+		System.out.println("getUserCode = " + userToken.getUserCode());
+		System.out.println("userToken    = " + userToken.getToken());
+		System.out.println("userToken2   = " + userToken2.getToken());
+		System.out.println("serviceToken = " + serviceToken.getToken());
 
 		QEventMessage initMsg = new QEventMessage("EVT_MSG", "INIT_STARTUP");
 
 		QEventMessage authInitMsg1 = new QEventMessage("EVT_MSG", "AUTH_INIT");
 		authInitMsg1.setToken(userToken.getToken());
-		QEventMessage authInitMsg2 = new QEventMessage("EVT_MSG", "AUTH_INIT");
-		authInitMsg2.setToken(userToken2.getToken());
-		QEventMessage msg1 = new QEventMessage("EVT_MSG", "INIT_1");
+//		QEventMessage authInitMsg2 = new QEventMessage("EVT_MSG", "AUTH_INIT");
+//		authInitMsg2.setToken(userToken2.getToken());
+//		QEventMessage msg1 = new QEventMessage("EVT_MSG", "INIT_1");
 		QEventMessage msgLogout1 = new QEventMessage("EVT_MSG", "LOGOUT");
 		msgLogout1.setToken(userToken.getToken());
-		QEventMessage msgLogout2 = new QEventMessage("EVT_MSG", "LOGOUT");
-		msgLogout2.setToken(userToken2.getToken());
+//		QEventMessage msgLogout2 = new QEventMessage("EVT_MSG", "LOGOUT");
+//		msgLogout2.setToken(userToken2.getToken());
 
 		List<Answer> answers = new ArrayList<Answer>();
 		answers.add(new Answer(userToken.getUserCode(), userToken.getUserCode(), "PRI_FIRSTNAME", "Bruce"));
@@ -91,6 +172,7 @@ public class ChrisTest {
 		answerMsg.setToken(userToken.getToken());
 
 		// NOW SET UP Some baseentitys
+		System.out.println("Setting up Base Entities");
 		BaseEntity project = new BaseEntity("PRJ_" + serviceToken.getRealm().toUpperCase(),
 				StringUtils.capitaliseAllWords(serviceToken.getRealm()));
 		project.setRealm(serviceToken.getRealm());
@@ -100,23 +182,38 @@ public class ChrisTest {
 		GennyKieSession gks = null;
 
 		try {
-			gks = GennyKieSession.builder(serviceToken, true).addDrl("SignalProcessing").addDrl("DataProcessing")
-					.addDrl("EventProcessing").addDrl("InitialiseProject").addJbpm("InitialiseProject")
-					.addJbpm("Lifecycles").addDrl("AuthInit").addJbpm("AuthInit")
-
-					.addToken(userToken).build();
+			gks = GennyKieSession.builder(serviceToken, true)
+					.addDrl("SignalProcessing")
+					.addDrl("DataProcessing")
+					.addDrl("EventProcessing")
+					.addDrl("InitialiseProject")
+					.addJbpm("InitialiseProject")
+					.addJbpm("Lifecycles")
+					.addDrl("AuthInit")
+					.addJbpm("AuthInit")
+					.addToken(userToken)
+					.build();
+			
 			gks.start();
-			gks.injectEvent(initMsg); // This should create a new process
+			
+			// This starts a new process which generates themes, frames, cards etc
+			gks.injectEvent(initMsg);
 
-			gks.injectEvent(authInitMsg1); // This should create a new process
+			// Inject "EVT_MSG", "AUTH_INIT" - This should create a new process
+			gks.injectEvent(authInitMsg1); 
+			
 			gks.advanceSeconds(5, false);
-			gks.advanceSeconds(5, false);
-			gks.injectEvent(authInitMsg1); // This should attach to existing process
-			gks.advanceSeconds(5, false);
-
-			gks.injectEvent(answerMsg); // This sends an answer to the first userSessio
-			gks.advanceSeconds(5, false);
+//			gks.advanceSeconds(5, false);
+//			gks.injectEvent(authInitMsg1); // This should attach to existing process
+//			gks.advanceSeconds(5, false);
+//			gks.injectEvent(answerMsg); // This sends an answer to the first userSessio
+			
+			gks.advanceSeconds(30, false);
+			
+			// Inject logout message 
 			gks.injectEvent(msgLogout1);
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 
