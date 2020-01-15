26a4ea77d3623f00db13bfe71527addb6daebb53
Adam Crow <adamcrow63@gmail.com>
2020-01-15 16:26:55 +1100

    Changed address conversion to handle missing address fields
    Former-commit-id: 462649bbea8672af6691518a5b341dd5039fa56e

--- src/test/java/life/genny/utils/JsonTest.java
+++ src/test/java/life/genny/utils/JsonTest.java
@@ -1,5 +1,8 @@
 package life.genny.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import io.vertx.core.json.JsonArray;
@@ -76,43 +79,53 @@ public void AddressTest()
 			"   ],\n" + 
 			"   \"status\" : \"OK\"\n" + 
 			"}";
-	JsonObject json = new JsonObject(addressJsonStr);
-	if ("OK".equals(json.getString("status"))) {
-		JsonObject results = json.getJsonArray("results").getJsonObject(0);
-		System.out.println(results);
-		JsonArray address_components = results.getJsonArray("address_components");
-		JsonObject streetNumberJson = address_components.getJsonObject(0);
-
-		JsonObject streetJson = address_components.getJsonObject(1);
-		String street_address = streetNumberJson.getString("short_name")+" "+streetJson.getString("short_name");
-		JsonObject cityJson = address_components.getJsonObject(2);
-		String suburb = cityJson.getString("short_name");
-		JsonObject countryJson = address_components.getJsonObject(5);
-		String country = countryJson.getString("short_name");
-		JsonObject stateJson = address_components.getJsonObject(4);
-		String state = stateJson.getString("short_name");
-
-		JsonObject postcodeJson = address_components.getJsonObject(6);
-		String postcode = postcodeJson.getString("short_name");
-		JsonObject geometry = results.getJsonObject("geometry");
+	
+	
+	String addressJsonStr2 = "{\"address_components\":[{\"long_name\":\"Tarneit\",\"short_name\":\"Tarneit\",\"types\":[\"locality\",\"political\"]},{\"long_name\":\"City of Wyndham\",\"short_name\":\"Wyndham\",\"types\":[\"administrative_area_level_2\",\"political\"]},{\"long_name\":\"Victoria\",\"short_name\":\"VIC\",\"types\":[\"administrative_area_level_1\",\"political\"]},{\"long_name\":\"Australia\",\"short_name\":\"AU\",\"types\":[\"country\",\"political\"]},{\"long_name\":\"3029\",\"short_name\":\"3029\",\"types\":[\"postal_code\"]}],\"formatted_address\":\"Tarneit VIC 3029, Australia\",\"geometry\":{\"bounds\":{\"northeast\":{\"lat\":-37.80094589999999,\"lng\":144.7139713},\"southwest\":{\"lat\":-37.87168399999999,\"lng\":144.614677}},\"location\":{\"lat\":-37.8091678,\"lng\":144.6672079},\"location_type\":\"APPROXIMATE\",\"viewport\":{\"northeast\":{\"lat\":-37.80094589999999,\"lng\":144.7139713},\"southwest\":{\"lat\":-37.87168399999999,\"lng\":144.614677}}},\"partial_match\":true,\"place_id\":\"ChIJwwOmKjL11moRcNiMIXVWBAU\",\"types\":[\"locality\",\"political\"]}";
+	
+	
+	JsonObject json = new JsonObject(addressJsonStr2);
+if (true || "OK".equals(json.getString("status"))) {
+	//	JsonObject results = json.getJsonArray("results").getJsonObject(0);
+	//	System.out.println(results);
+		JsonArray address_components = json.getJsonArray("address_components");
+		
+		/* loop through address_components */
+		Integer index = 0;
+		Integer count = address_components.size();
+		Map<String,String> addressMap = new HashMap<String,String>();
+		for (index=0;index<count;index++) {
+			
+			JsonObject addressComponent = address_components.getJsonObject(index);
+			JsonArray types = addressComponent.getJsonArray("types");
+			String mainType = types.getString(0);
+			addressMap.put(mainType, addressComponent.getString("short_name"));
+		}
+		
+		String streetNumber = (addressMap.get("street_number")==null)?"":addressMap.get("street_number");
+		String streetName = (addressMap.get("route")==null)?"":addressMap.get("route");
+		
+		String street_address = (streetNumber+" "+streetName).trim();
+		
+		JsonObject geometry = json.getJsonObject("geometry");
 		JsonObject location = geometry.getJsonObject("location");
 		Double lat = location.getDouble("lat");
 		Double lng = location.getDouble("lng");
-		String full_address = results.getString("formatted_address");
+		String full_address = json.getString("formatted_address");
 		System.out.println(address_components);
 		
 		JsonObject address_json = new JsonObject();
 		address_json.put("street_address", street_address);
-		address_json.put("suburb", suburb);
-		address_json.put("state", state);
-		address_json.put("country", country);
-		address_json.put("postcode", postcode);
+		address_json.put("suburb", (addressMap.get("locality")==null?"":addressMap.get("locality")));
+		address_json.put("state",  (addressMap.get("administrative_area_level_1")==null?"":addressMap.get("administrative_area_level_1")));
+		address_json.put("country",  (addressMap.get("country")==null?"":addressMap.get("country")));
+		address_json.put("postcode",  (addressMap.get("postal_code")==null?"":addressMap.get("postal_code")));
 		address_json.put("full_address", full_address);
 		address_json.put("latitude", lat);
 		address_json.put("longitude", lng);
 		
 		String PRI_ADDRESS_JSON = address_json.toString();
-		System.out.println("PRI_ADDRESS_JSON="+PRI_ADDRESS_JSON);
+		System.out.println("PRI_ADDRESS_JSON="+PRI_ADDRESS_JSON);			
 
 	}
 }
