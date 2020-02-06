9e119bacfaccd803acc313b2cb945cc9b88dad3f
Jasper <jasper.robison22@gmail.com>
2020-02-06 14:55:22 +1100

    cache and grab context
    Former-commit-id: 10d35117ef5de972d7831ae75b6f225197e09a4a

--- src/test/java/life/genny/test/JasperTest.java
+++ src/test/java/life/genny/test/JasperTest.java
@@ -1,6 +1,7 @@
 package life.genny.test;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -20,11 +21,13 @@ import javax.jms.TextMessage;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.logging.log4j.Logger;
 import org.codehaus.plexus.util.StringUtils;
 import org.jbpm.services.task.utils.TaskFluent;
 import org.jbpm.services.task.wih.NonManagedLocalHTWorkItemHandler;
 import org.jbpm.workflow.instance.WorkflowProcessInstance;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -37,11 +40,19 @@ import org.kie.internal.task.api.model.InternalComment;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 
+import io.vertx.core.json.JsonObject;
+
 import life.genny.eventbus.EventBusInterface;
 import life.genny.eventbus.EventBusMock;
 import life.genny.eventbus.VertxCache;
 import life.genny.models.GennyToken;
 import life.genny.qwanda.Answer;
+import life.genny.qwanda.Ask;
+import life.genny.qwanda.Context;
+import life.genny.qwanda.ContextList;
+import life.genny.qwanda.ContextType;
+import life.genny.qwanda.Question;
+import life.genny.qwanda.VisualControlType;
 import life.genny.qwanda.entity.BaseEntity;
 import life.genny.qwanda.exception.BadDataException;
 import life.genny.qwanda.message.QDataAnswerMessage;
@@ -49,12 +60,17 @@ import life.genny.qwanda.message.QEventMessage;
 import life.genny.qwandautils.GennyCacheInterface;
 import life.genny.qwandautils.GennySettings;
 import life.genny.qwandautils.JsonUtils;
+import life.genny.qwandautils.QwandaMessage;
 import life.genny.rules.QRules;
+import life.genny.utils.BaseEntityUtils;
 import life.genny.utils.GennyJbpmBaseTest;
 import life.genny.utils.GennyKieSession;
+import life.genny.utils.QuestionUtils;
 import life.genny.utils.RulesUtils;
 import life.genny.utils.SessionFacts;
 import life.genny.utils.VertxUtils;
+
+import life.genny.bootxport.bootx.GoogleImportService;
 
 public class JasperTest {
 
@@ -86,6 +102,111 @@ public class JasperTest {
 	}
 	
 	@Test
+	public void TestTest() throws ClientProtocolException, IOException {
+		GennyToken userToken = null;
+        GennyToken serviceToken = null;
+        QRules qRules = null;
+
+        if (false) {
+            userToken = GennyJbpmBaseTest.createGennyToken(realm, "user1", "Barry Allan", "user");
+            serviceToken = GennyJbpmBaseTest.createGennyToken(realm, "service", "Service User", "service");
+            qRules = new QRules(eventBusMock, userToken.getToken());
+            qRules.set("realm", userToken.getRealm());
+            qRules.setServiceToken(serviceToken.getToken());
+            VertxUtils.cachedEnabled = true; // don't send to local Service Cache
+        } else {
+            VertxUtils.cachedEnabled = false; // don't send to local Service Cache
+            qRules = GennyJbpmBaseTest.setupLocalService();
+            userToken = new GennyToken("userToken", qRules.getToken());
+            serviceToken = new GennyToken("PER_SERVICE", qRules.getServiceToken());
+        }
+		
+		/* Get the ask we are looking for */		
+		QwandaMessage msg = QuestionUtils.getQuestions("PER_USER1", "PER_USER1", "QUE_STT_S1_A_Q1_GRP", serviceToken.getToken());
+		Ask askParent = msg.asks.getItems()[0];
+		
+		/* get the specific child question from that ask */
+		Ask ask = askParent.getChildAsks()[0];
+		
+		/* construct the Context */
+		Context context = new Context(ContextType.UNITY, "UNT_S1_DATA");
+		
+		ContextList contextList = ask.getContextList();
+		List<Context> ctxList = contextList.getContextList();
+		ctxList.add(context);
+	
+		contextList.setContextList(ctxList);
+		ask.setContextList(contextList);
+		
+		JsonObject askObj = new JsonObject(JsonUtils.toJson(ask));
+		
+//		/* submit that context */
+//		QuestionUtils.addContextToAsk(ask, context, serviceToken);
+//		
+//		
+//		String questionCode = "QUE_STT_S1_A_Q1_GRP";
+//		
+//		/* Get the ask we are looking for */		
+//		QwandaMessage msg2 = QuestionUtils.getQuestions("PER_USER1", "PER_USER1", questionCode, serviceToken.getToken());
+//		Ask ask2 = msg.asks.getItems()[0];
+//		System.out.println("Ask qCode: " + ask2.getQuestionCode());
+//		
+//		/* get the specific child question from that ask */
+//		Ask childAsk2 = ask.getChildAsks()[0];
+//		System.out.println("chldAsk qCode: " + childAsk2.getQuestionCode());
+//		
+//		System.out.println("ask2: " + ask2.getContextList());
+//		System.out.println("ChildAsk2: " + childAsk2.getContextList());
+		
+		
+//		VertxUtils.writeCachedJson(serviceToken.getRealm(), ask.getQuestionCode(), askObj.toString(), serviceToken.getToken());
+//
+//
+//
+//
+//		JsonObject askJson = VertxUtils.readCachedJson(serviceToken.getRealm(), ask.getQuestionCode(), serviceToken.getToken());
+//		
+//		Ask newAsk = JsonUtils.fromJson(askJson.getString("value"), life.genny.qwanda.Ask.class);
+//		
+//		System.out.println("CACHED ASK: " + newAsk);
+//		System.out.println("CACHED ASK CONTEXTS: " + newAsk.getContextList());
+        
+        BaseEntityUtils beUtils = new BaseEntityUtils(serviceToken);		
+                
+        
+        JsonObject ctxJson = VertxUtils.readCachedJson(serviceToken.getRealm(), "CTX_UNITY_CONTEXT" , serviceToken.getToken());
+		System.out.println("CTX JSON: " + ctxJson);
+//		JsonObject contextJson = new JsonObject(ctxJson.getString("value"));
+//		Context ctx = JsonUtils.fromJson(ctxJson.getString("value"), life.genny.qwanda.Context.class);
+//		System.out.println("CTX CODE: " + ctx.getContextCode());
+//		BaseEntity unityBe = BaseEntityUtils.getBaseEntityByCode(ctx.getContextCode());
+//		System.out.println("CTX Entity: " + ctx.getEntity());
+//		ctx.setEntity(unityBe);
+        
+        
+//        String ctxJson = JsonUtils.toJson(context);
+//        System.out.println(ctxJson);
+//        
+//        JsonObject obj = new JsonObject();//.remove("realm");	3
+//        obj.put("weight", 1.0);
+//        obj.put("contextCode", "UNT_S1_DATA");
+//        obj.put("visualControlType", "VCL_DEFAULT");
+//        obj.put("created", "2020-02-02T23:45:19.405");
+//        obj.put("name", "UNITY");
+//        obj.put("realm", "genny");
+        
+        /*
+        String test = "{ \"weight\": 1.0,\"contextCode\": \"UNT_S1_DATA\",\"visualControlType\": \"VCL_DEFAULT\",\"created\": \"2020-02-03T00:08:47.258\", \"name\": \"UNITY\",\"realm\": \"genny\" }";
+        
+        Context ctx = JsonUtils.fromJson(test, life.genny.qwanda.Context.class);
+        
+        System.out.println("ctx: " + ctx);
+        
+        */
+        
+	}
+	
+	//@Test
     public void LifecycleTest() {
         GennyToken userToken = null;
         GennyToken serviceToken = null;
@@ -189,7 +310,7 @@ public class JasperTest {
         System.out.println("All messages received!");
     }
 	
-    @BeforeClass
+    //@BeforeClass
 	public static void init() throws FileNotFoundException, SQLException {
 
 		System.out.println("BridgeUrl=" + GennySettings.bridgeServiceUrl);
