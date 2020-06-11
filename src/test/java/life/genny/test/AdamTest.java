ce42d57ac9510fef07023d049200642894669cde
Adam Crow <adamcrow63@gmail.com>
2020-06-12 07:01:59 +1000

    Updated ada,test
    Former-commit-id: 4cb721a6a02d80545723fadf4bcefe422316c9a2

--- src/test/java/life/genny/test/AdamTest.java
+++ src/test/java/life/genny/test/AdamTest.java
@@ -200,95 +200,120 @@ public class AdamTest {
 		QDataBaseEntityMessage msg = null;
 		SearchEntity searchBE = VertxUtils.getObject(realm, "", "SBE_EDU_PROVIDERS_ACTIVE", SearchEntity.class, serviceToken.getToken());
 		long starttime = System.currentTimeMillis();
+		long endtime = 0;
 
 		String beFilter1 = null;
 		String beFilter2 = null;
-		String attributeFilterValue1="";
+		String attributeFilterValue1 = "";
 		String attributeFilterCode1 = null;
-		String attributeFilterValue2="";
+		String attributeFilterValue2 = "";
 		String attributeFilterCode2 = null;
-		Integer pageStart = searchBE.getValue("SCH_PAGE_START",0);
-		Integer pageSize = searchBE.getValue("SCH_PAGE_SIZE",GennySettings.defaultPageSize);
-		
-			List<String> attributeFilter = new ArrayList<String>();
-			for (EntityAttribute ea : searchBE.getBaseEntityAttributes()) {
-				
-				if (ea.getAttributeCode().startsWith("PRI_CODE")) {
-					if (beFilter1==null) {
-						beFilter1 = ea.getAsString();
-					} else if (beFilter2==null) {
-						beFilter2 = ea.getAsString();
+		String sortCode = "eb";
+		String sortValue = "ASC";
+		Integer pageStart = searchBE.getValue("SCH_PAGE_START", 0);
+		Integer pageSize = searchBE.getValue("SCH_PAGE_SIZE", GennySettings.defaultPageSize);
+
+		List<String> attributeFilter = new ArrayList<String>();
+		for (EntityAttribute ea : searchBE.getBaseEntityAttributes()) {
+
+			if (ea.getAttributeCode().startsWith("PRI_CODE")) {
+				if (beFilter1 == null) {
+					beFilter1 = ea.getAsString();
+				} else if (beFilter2 == null) {
+					beFilter2 = ea.getAsString();
+				}
+			} else if ((ea.getAttributeCode().startsWith("SRT_"))) {
+				sortCode = (ea.getAttributeCode().substring("SRT_".length()));
+				sortValue = ea.getValueString();
+
+			} else if ((ea.getAttributeCode().startsWith("COL_")) || (ea.getAttributeCode().startsWith("CAL_"))) {
+				attributeFilter.add(ea.getAttributeCode().substring("COL_".length()));
+
+			} else if (ea.getAttributeCode().startsWith("PRI_") && (!ea.getAttributeCode().equals("PRI_CODE"))) {
+				if (attributeFilterCode1 == null) {
+					if (ea.getValueString() != null) {
+						attributeFilterValue1 = " eb.valueString " + ea.getAttributeName() + " '"
+								+ ea.getValueString() + "'";
+					} else if (ea.getValueBoolean() != null) {
+						attributeFilterValue1 = " eb.valueBoolean = " + (ea.getValueBoolean() ? "true" : "false");
 					}
-					
-				} else if ((ea.getAttributeCode().startsWith("COL_")) || (ea.getAttributeCode().startsWith("CAL_"))){
-					attributeFilter.add(ea.getAttributeCode().substring("COL_".length()));
-					
-				} else if (ea.getAttributeCode().startsWith("PRI_")&&(!ea.getAttributeCode().equals("PRI_CODE"))) {
-					if (attributeFilterCode1==null) {
-						if (ea.getValueString()!=null) {
-							attributeFilterValue1 = " eb.valueString "+ea.getAttributeName()+" '"+ea.getValueString()+"'";
-						} else if (ea.getValueBoolean()!=null) {
-							attributeFilterValue1 = " eb.valueBoolean = "+(ea.getValueBoolean()?"true":"false");
-						} 
-						attributeFilterCode1 = ea.getAttributeCode();
-						
-					} else {
-						if (attributeFilterCode2==null) {
-							if (ea.getValueString()!=null) {
-								attributeFilterValue2 = " ec.valueString "+ea.getAttributeName()+" '"+ea.getValueString()+"'";
-							} else if (ea.getValueBoolean()!=null) {
-								attributeFilterValue2 = " ec.valueBoolean = "+(ea.getValueBoolean()?"true":"false");
-							} 
-							attributeFilterCode2 = ea.getAttributeCode();
+					attributeFilterCode1 = ea.getAttributeCode();
+
+				} else {
+					if (attributeFilterCode2 == null) {
+						if (ea.getValueString() != null) {
+							attributeFilterValue2 = " ec.valueString " + ea.getAttributeName() + " '"
+									+ ea.getValueString() + "'";
+						} else if (ea.getValueBoolean() != null) {
+							attributeFilterValue2 = " ec.valueBoolean = "
+									+ (ea.getValueBoolean() ? "true" : "false");
 						}
+						attributeFilterCode2 = ea.getAttributeCode();
 					}
 				}
 			}
-			String hql = "select distinct ea.baseEntityCode from EntityAttribute ea ";
-				hql +=	 ", EntityAttribute eb ";
-		 		if (attributeFilterCode2!=null) {
-		 			hql +=	 ", EntityAttribute ec ";
-		 		}
-			hql +=   " where ea.baseEntityCode=eb.baseEntityCode ";
-	 		hql += " and ea.baseEntityCode like '"+beFilter1+"'  ";
-	 	//	hql += " or ea.baseEntityCode like '"+beFilter2+"')  ";
-	 		hql += " and eb.attributeCode = '"+attributeFilterCode1+"' and "+attributeFilterValue1;
-	 		if (attributeFilterCode2!=null) {
-	 			hql += " and ec.attributeCode = '"+attributeFilterCode2+"' and "+attributeFilterValue2;
-	 		}
+		}
+		String hql = "select distinct ea.baseEntityCode from EntityAttribute ea ";
+		hql += ", EntityAttribute eb ";
+		if (attributeFilterCode2 != null) {
+			hql += ", EntityAttribute ec ";
+		}
+		hql += " where ea.baseEntityCode=eb.baseEntityCode ";
+		hql += " and (ea.baseEntityCode like '" + beFilter1 + "'  ";
+		hql += " or ea.baseEntityCode like '" + beFilter2 + "')  ";
+		hql += " and eb.attributeCode = '" + attributeFilterCode1 + "' and " + attributeFilterValue1;
+		if (attributeFilterCode2 != null) {
+			hql += " and ea.baseEntityCode=ec.baseEntityCode ";
+			hql += " and ec.attributeCode = '" + attributeFilterCode2 + "' and " + attributeFilterValue2;
+		}
+	//	hql += " order by " + sortCode + " " + sortValue;
 
-	 		
-	 		String hql2 = Base64.getUrlEncoder().encodeToString(hql.getBytes());
-			JsonObject resultJson;
-			QDataBaseEntityMessage resultMsg = new QDataBaseEntityMessage();
-			String realm = serviceToken.getRealm();
-			resultMsg.setToken(beUtils.getGennyToken().getToken());
-			try {
-				String resultJsonStr = QwandaUtils.apiGet(GennySettings.qwandaServiceUrl + "/qwanda/baseentitys/search24/"+hql2+"/"+pageStart+"/"+pageSize, serviceToken.getToken(),120);
-				
-				resultJson = new JsonObject(resultJsonStr);
-				
-				Type listType = new TypeToken<List<String>>() {
-				}.getType();
-				Long total = resultJson.getLong("total");
-				JsonArray result = resultJson.getJsonArray("codes");
-				List<String> resultCodes = new ArrayList<String>();
-				for(int i = 0; i < result.size(); i++){
-				    String code = result.getString(i);
-				   resultCodes.add(code);
-				}
-				String[] filterArray = attributeFilter.toArray(new String[0]);
-				List<BaseEntity> beList = resultCodes.stream().map(e -> {
-					BaseEntity be = beUtils.getBaseEntityByCode(e); 
-					be = VertxUtils.privacyFilter(be, filterArray);
-					return be;
-					}).collect(Collectors.toList());
-				resultMsg.setItems(beList.toArray(new BaseEntity[0]));
-				resultMsg.setTotal(total);
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}		
-			long endtime = System.currentTimeMillis();
+		String hql2 = Base64.getUrlEncoder().encodeToString(hql.getBytes());
+		JsonObject resultJson;
+		try {
+			String resultJsonStr = QwandaUtils.apiGet(GennySettings.qwandaServiceUrl
+					+ "/qwanda/baseentitys/search24/" + hql2 + "/" + pageStart + "/" + pageSize,
+					serviceToken.getToken(), 120);
+
+			String coreSearchCode = StringUtils.removeEnd(searchBE.getCode(),
+					beUtils.getGennyToken().getSessionCode());
+			resultJson = new JsonObject(resultJsonStr);
+			Long total = 1000L; // resultJson.getLong("total");
+			// check the cache
+			JsonObject countJson = VertxUtils.readCachedJson(serviceToken.getRealm(), "COUNT_" + coreSearchCode,
+					serviceToken.getToken());
+			String countJsonStr = null;
+			if ("OK".equalsIgnoreCase(countJson.getString("status"))) {
+				countJsonStr = countJson.getString("value");
+			}
+			total = 1964L;
+			if (countJsonStr == null) {
+				countJsonStr = QwandaUtils.apiGet(
+						GennySettings.qwandaServiceUrl + "/qwanda/baseentitys/search24/" + hql2,
+						serviceToken.getToken(), 120);
+				VertxUtils.writeCachedJson(serviceToken.getRealm(), "COUNT_" + coreSearchCode, countJsonStr,
+						serviceToken.getToken());
+			}
+			total = Long.parseLong(countJsonStr);
+
+			JsonArray result = resultJson.getJsonArray("codes");
+			List<String> resultCodes = new ArrayList<String>();
+			for (int i = 0; i < result.size(); i++) {
+				String code = result.getString(i);
+				resultCodes.add(code);
+			}
+			String[] filterArray = attributeFilter.toArray(new String[0]);
+			List<BaseEntity> beList = resultCodes.stream().map(e -> {
+				BaseEntity be = beUtils.getBaseEntityByCode(e);
+				be = VertxUtils.privacyFilter(be, filterArray);
+				return be;
+			}).collect(Collectors.toList());
+			msg = new QDataBaseEntityMessage(beList.toArray(new BaseEntity[0]));
+			msg.setTotal(total);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+			endtime = System.currentTimeMillis();
 			System.out.println("Total time taken = "+(endtime-starttime)+" ms");
 	}
 	
