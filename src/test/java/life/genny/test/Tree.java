139d3b0a137eec0b2f30f4926d79d8546ae3c65a
ooanishoo <anish.maharjan12341@gmail.com>
2020-07-30 16:57:47 +1000

    update test
    Former-commit-id: 33cef0fe836096972c0d2307e1d57fa207a1db41

--- src/test/java/life/genny/test/Tree.java
+++ src/test/java/life/genny/test/Tree.java
@@ -96,25 +96,96 @@ public class Tree extends GennyJbpmBaseTest {
 		
 		Question processViewQues = new Question("QUE_TAB_BUCKET_VIEW", "Process View", eventAttribute, true);
 		Ask processViewAsk = new Ask(processViewQues, sourceCode, targetCode);
-		
-		Question contactsQues = new Question("QUE_TREE_ITEM_CONTACTS_GRP", "Contacts", questionAttribute, true);
-		Ask contactsAsk = new Ask(contactsQues, sourceCode, targetCode);
-		
+
+		/* internships */
 		Question internshipsQues = new Question("QUE_TREE_ITEM_INTERNSHIPS_GRP", "Internships", questionAttribute, true);
 		Ask internshipsAsk = new Ask(internshipsQues, sourceCode, targetCode);
-		
+
+			Question activeInternshipsQues = new Question("QUE_TREE_ITEM_INTERNSHIPS_ACTIVE", "Active", eventAttribute, true);
+			Ask activeInternshipsAsk = new Ask(activeInternshipsQues, sourceCode, targetCode);
+
+			Ask[] internshipChildAsks = { activeInternshipsAsk };
+			internshipsAsk.setChildAsks(internshipChildAsks);
+
+		/* host-companies */
 		Question hostCompaniesQues = new Question("QUE_TREE_ITEM_HOST_COMPANIES_GRP", "Host Companies", questionAttribute, true);
 		Ask hostCompaniesAsk = new Ask(hostCompaniesQues, sourceCode, targetCode);
+
+			Question hostCompaniesActiveQues = new Question("QUE_TREE_ITEM_HOST_COMPANIES_ACTIVE", "Active", eventAttribute, true);
+			Question hostCompaniesOnHoldQues = new Question("QUE_TREE_ITEM_HOST_COMPANIES_ON_HOLD", "On Hold", eventAttribute, true);
+			Question hostCompaniesWithdrawnQues = new Question("QUE_TREE_ITEM_HOST_COMPANIES_WITHDRAWN", "Withdrawn", eventAttribute, true);
+			
+			Ask hostCompaniesActiveAsk = new Ask(hostCompaniesActiveQues, sourceCode, targetCode);
+			Ask hostCompaniesOnHoldAsk = new Ask(hostCompaniesOnHoldQues, sourceCode, targetCode);
+			Ask hostCompaniesWithdrawnAsk = new Ask(hostCompaniesWithdrawnQues, sourceCode, targetCode);
+
+			Ask[] hostCompaniesChildAsks = { hostCompaniesActiveAsk, hostCompaniesOnHoldAsk, hostCompaniesWithdrawnAsk };
+			hostCompaniesAsk.setChildAsks(hostCompaniesChildAsks);
 		
+		/* edu-providers */
 		Question eduProvidersQues = new Question("QUE_TREE_ITEM_EDU_PROVIDERS_GRP", "Edu Providers", questionAttribute, true);
 		Ask eduProvidersAsk = new Ask(eduProvidersQues, sourceCode, targetCode);
+
+			Question eduProActiveQues = new Question("QUE_TREE_ITEM_EDU_PROVIDERS_ACTIVE", "Active", eventAttribute, true);
+			Question eduProOnHoldQues = new Question("QUE_TREE_ITEM_EDU_PROVIDERS_ON_HOLD", "On Hold", eventAttribute, true);
+			Question eduProWithdrawnQues = new Question("QUE_TREE_ITEM_EDU_PROVIDERS_WITHDRAWN", "Withdrawn", eventAttribute, true);
+			
+			Ask eduProActiveAsk = new Ask(eduProActiveQues, sourceCode, targetCode);
+			Ask eduProOnHoldAsk = new Ask(eduProOnHoldQues, sourceCode, targetCode);
+			Ask eduProWithdrawnAsk = new Ask(eduProWithdrawnQues, sourceCode, targetCode);
+
+			Ask[] eduProChildAsks = { eduProActiveAsk, eduProOnHoldAsk, eduProWithdrawnAsk };
+			hostCompaniesAsk.setChildAsks(eduProChildAsks);
+
+		/* people */
+		Question contactsQues = new Question("QUE_TREE_ITEM_CONTACTS_GRP", "Contacts", questionAttribute, true);
+		Ask contactsAsk = new Ask(contactsQues, sourceCode, targetCode);
+
+			Question internsQues = new Question("QUE_TREE_ITEM_INTERNS", "Interns", eventAttribute, true);
+			Question hcrQues = new Question("QUE_TREE_ITEM_HCR", "Host Company Reps", eventAttribute, true);
+			Question eduProRepQues = new Question("QUE_TREE_ITEM_EPR", "Edu Provider Reps", eventAttribute, true);
+			Question agentsQues = new Question("QUE_TREE_ITEM_AGENTS", "Agents", eventAttribute, true);
+			Question referrersQues = new Question("QUE_TREE_ITEM_REFERRERS", "Referrers", eventAttribute, true);
+			
+			Ask internsAsk = new Ask(internsQues, sourceCode, targetCode);
+			Ask hcrAsk = new Ask(hcrQues, sourceCode, targetCode);
+			Ask eduProRepAsk = new Ask(eduProRepQues, sourceCode, targetCode);
+			Ask agentsAsk = new Ask(agentsQues, sourceCode, targetCode);
+			Ask referrersAsk = new Ask(referrersQues, sourceCode, targetCode);
+
+			Ask[] contactsChildAsks = { internsAsk, hcrAsk, eduProRepAsk, agentsAsk, referrersAsk };
+			contactsAsk.setChildAsks(contactsChildAsks);
 		
-		Ask[] sidebarItems = { dashboardAsk, processViewAsk };
-		sidebarAsk.setChildAsks(sidebarItems);
+		Ask[] agentSidebar = { dashboardAsk, processViewAsk, internshipsAsk, contactsAsk, hostCompaniesAsk, eduProvidersAsk };
+		Ask[] internSidebar = { dashboardAsk, processViewAsk, internshipsAsk };
+		Ask[] eduProRepSidebar = { dashboardAsk, processViewAsk, eduProRepAsk, internsAsk };
+		Ask[] hcRepSidebar = { dashboardAsk, processViewAsk, internshipsAsk, hcrAsk };
 		
+		sidebarAsk.setChildAsks(agentSidebar);
 		VertxUtils.putObject(serviceToken.getRealm(), "", "SIDEBAR_AGENT", sidebarAsk, serviceToken.getToken());
 
-		Ask cachedAsk = VertxUtils.getObject(serviceToken.getRealm(), "", "SIDEBAR_AGENT", Ask.class, serviceToken.getToken());
+		sidebarAsk.setChildAsks(internSidebar);
+		VertxUtils.putObject(serviceToken.getRealm(), "", "SIDEBAR_INTERN", sidebarAsk, serviceToken.getToken());
+
+		sidebarAsk.setChildAsks(eduProRepSidebar);
+		VertxUtils.putObject(serviceToken.getRealm(), "", "SIDEBAR_EDU_PRO_REP", sidebarAsk, serviceToken.getToken());
+
+		sidebarAsk.setChildAsks(hcRepSidebar);
+		VertxUtils.putObject(serviceToken.getRealm(), "", "SIDEBAR_HOST_CPY_REP", sidebarAsk, serviceToken.getToken());
+		
+		Ask askAgent = VertxUtils.getObject(serviceToken.getRealm(), "", "SIDEBAR_AGENT", Ask.class, serviceToken.getToken());
+		Ask askIntern = VertxUtils.getObject(serviceToken.getRealm(), "", "SIDEBAR_INTERN", Ask.class, serviceToken.getToken());
+		Ask askEduRep = VertxUtils.getObject(serviceToken.getRealm(), "", "SIDEBAR_EDU_PRO_REP", Ask.class, serviceToken.getToken());
+		Ask askHcr = VertxUtils.getObject(serviceToken.getRealm(), "", "SIDEBAR_HOST_CPY_REP", Ask.class, serviceToken.getToken());
+		
+		Ask[] asks = new Ask[1];
+		asks[0] = askAgent;
+		
+		QDataAskMessage askMsg = new QDataAskMessage(askAgent);
+		askMsg.setItems(asks);
+		askMsg.setToken(userToken.getToken());
+		VertxUtils.writeMsg("webcmds", askMsg);
+		
 		System.out.println("end");
 
 	}
