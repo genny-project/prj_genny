43f05f96ecf7557d550bc51c958c2cd495f0a6fa
Adam Crow <adamcrow63@gmail.com>
2021-08-06 10:43:31 +1000

    Added AppointmentsTest

---
+++ src/test/java/life/genny/utils/AppointmentUtils.java
@@ -0,0 +1,22 @@
+package life.genny.utils;
+
+import java.lang.invoke.MethodHandles;
+import java.util.logging.Logger;
+
+public class AppointmentUtils {
+
+	protected static final Logger log = org.apache.logging.log4j.LogManager
+			.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
+
+	BaseEntityUtils beUtils = null;
+
+	public AppointmentUtils(BaseEntityUtils beUtils) {
+		this.beUtils = beUtils;
+	}
+	
+	public Boolean getBoolean()
+	{
+		return true;
+	}
+
+}
