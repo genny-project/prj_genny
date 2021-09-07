5ab702d6765baca07835fb20ce1514a74c77930f
ssar0014 <subhasish.sarkar93@gmail.com>
2021-09-07 12:09:48 +1000

    working tables, need to fix actions/events

--- src/test/java/life/genny/test/RemoteServiceTest.java
+++ src/test/java/life/genny/test/RemoteServiceTest.java
@@ -5,13 +5,16 @@ import java.io.StringReader;
 import java.sql.SQLException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 import io.vertx.core.json.JsonObject;
+import life.genny.bootxport.bootx.DEFBaseentityAttribute;
 import life.genny.eventbus.EventBusInterface;
 import life.genny.eventbus.EventBusMock;
 import life.genny.eventbus.VertxCache;
 import life.genny.qwanda.*;
 import life.genny.qwanda.attribute.AttributeText;
+import life.genny.qwanda.message.QEventBtnClickMessage;
 import life.genny.qwandautils.*;
 import life.genny.utils.*;
 import org.apache.commons.lang.StringUtils;
@@ -25,7 +28,9 @@ import life.genny.qwanda.entity.SearchEntity;
 import life.genny.qwanda.exception.BadDataException;
 import life.genny.qwanda.message.QCmdTableMessage;
 
+import org.dmg.pmml.Entity;
 import org.dmg.pmml.True;
+import org.infinispan.client.hotrod.Search;
 import org.jboss.logging.Logger;
 import org.jbpm.services.api.DefinitionService;
 import org.jbpm.services.api.ProcessService;
@@ -36,6 +41,7 @@ import org.jbpm.services.api.model.DeploymentUnit;
 import org.jbpm.services.api.query.QueryService;
 import org.jbpm.services.api.utils.KieServiceConfigurator;
 import org.json.JSONObject;
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -200,11 +206,11 @@ public class RemoteServiceTest {
         String authToken = userToken.getToken();
 
         // create 5 remote services
-        BaseEntity remoteServiceTest1 = createRemoteService("RMS_JNL_NLP_01", "NLP Journals", "http://localhost:5000/api/response","SBE_AI_JOURNAL");
-        BaseEntity remoteServiceTest2 = createRemoteService("RMS_HC_RECSYS_01", "Recommended Host Companies", "http://localhost:5001/api/response","SBE_AI_HC_RECSYS");
-        BaseEntity remoteServiceTest3 = createRemoteService("RMS_JNL_NLP_02", "NLP Journals Sub Research", "http://10.12.13.1:5002/api/response","SBE_AI_JOURNAL_2");
-        BaseEntity remoteServiceTest4 = createRemoteService("RMS_INTERN_RECSYS_02", "Recommended Interns", "http://10.12.13.1:5003/api/response","SBE_AI_INTERN_RECSYS2");
-        BaseEntity remoteServiceTest5 = createRemoteService("RMS_INTERN_CRED_01", "ID Intern Credentials", "http://ai.gada.io/interns/cred/api/response","SBE_AI_INTERN_CRED");
+        BaseEntity remoteServiceTest1 = createRemoteService("RMS_JNL_NLP_01", "NLP Journals", "http://localhost:5000/api/response","PER_3F1B3AC3-F8AA-4C20-B85D-4D43C4E2BF7D", "SBE_AI_JOURNAL");
+        BaseEntity remoteServiceTest2 = createRemoteService("RMS_HC_RECSYS_01", "Recommended Host Companies", "http://localhost:5001/api/response", "PER_3F1B3AC3-F8AA-4C20-B85D-4D43C4E2BF7D","SBE_AI_JOURNAL");
+        BaseEntity remoteServiceTest3 = createRemoteService("RMS_JNL_NLP_02", "NLP Journals Sub Research", "http://10.12.13.1:5002/api/response", "PER_3F1B3AC3-F8AA-4C20-B85D-4D43C4E2BF7D","SBE_AI_JOURNAL");
+        BaseEntity remoteServiceTest4 = createRemoteService("RMS_INTERN_RECSYS_02", "Recommended Interns", "http://10.12.13.1:5003/api/response","PER_3F1B3AC3-F8AA-4C20-B85D-4D43C4E2BF7D","SBE_AI_JOURNAL");
+        BaseEntity remoteServiceTest5 = createRemoteService("RMS_INTERN_CRED_01", "ID Intern Credentials", "http://ai.gada.io/interns/cred/api/response","PER_3F1B3AC3-F8AA-4C20-B85D-4D43C4E2BF7D","SBE_AI_JOURNAL");
 
 //      Search Entity to be displayed as a table
         SearchEntity searchRemoteServices = new SearchEntity("SBE_REMOTE_SERVICES", "Remote AI Services")
@@ -212,12 +218,17 @@ public class RemoteServiceTest {
                 .addFilter("PRI_CODE", SearchEntity.StringFilter.LIKE, "RMS_%")
                 .addColumn("PRI_NAME", "Name")
                 .addColumn("PRI_URL", "URL")
-                .addAssociatedColumn("LNK_AUTHOR", "PRI_NAME", "Author Name");
+                .addColumn("PRI_DESCRIPTION", "Description")
+                .addAssociatedColumn("LNK_AUTHOR", "PRI_NAME", "Author Name")
+                /* Row Action */
+                .addRowAction("PRI_EVENT_VIEW_REMOTE_JOBS", "View Remote Jobs");
         searchRemoteServices.setRealm(realm);
         searchRemoteServices.setPageStart(0);
         searchRemoteServices.setPageSize(1000);
 
-        beUtils.saveBaseEntity(searchRemoteServices);
+        VertxUtils.putObject(beUtils.getGennyToken().getRealm(), "", searchRemoteServices.getCode(), searchRemoteServices,
+                beUtils.getGennyToken().getToken());
+//        beUtils.saveBaseEntity(searchRemoteServices);
 
         List<BaseEntity> remoteServiceBES = beUtils.getBaseEntitys(searchRemoteServices);
 
@@ -225,28 +236,52 @@ public class RemoteServiceTest {
         String searchCode = "SBE_REMOTE_SERVICES";
         System.out.println("searchCode  ::  " + searchCode);
         TableUtils tableUtils = new TableUtils(beUtils);
-        SearchEntity searchBE = tableUtils.getSessionSearch(searchCode);
+        SearchEntity searchBE = searchRemoteServices;
+//        SearchEntity searchBE = tableUtils.getSessionSearch(searchCode);
         if(searchBE != null) {
             /* Reset page to start */
-            Answer pageAnswer = new Answer(beUtils.getGennyToken().getUserCode(), searchBE.getCode(),
-                    "SCH_PAGE_START", "0");
-            Answer pageNumberAnswer = new Answer(beUtils.getGennyToken().getUserCode(), searchBE.getCode(),
-                    "PRI_INDEX", "1");
+//            Answer pageAnswer = new Answer(beUtils.getGennyToken().getUserCode(), searchBE.getCode(),
+//                    "SCH_PAGE_START", "0");
+//            Answer pageNumberAnswer = new Answer(beUtils.getGennyToken().getUserCode(), searchBE.getCode(),
+//                    "PRI_INDEX", "1");
 
-            searchBE = beUtils.updateBaseEntity(searchBE, pageAnswer, SearchEntity.class);
-            searchBE = beUtils.updateBaseEntity(searchBE, pageNumberAnswer, SearchEntity.class);
+//            searchBE = beUtils.updateBaseEntity(searchBE, pageAnswer, SearchEntity.class);
+//            searchBE = beUtils.updateBaseEntity(searchBE, pageNumberAnswer, SearchEntity.class);
 
             VertxUtils.putObject(beUtils.getGennyToken().getRealm(), "", searchBE.getCode(), searchBE,
                     beUtils.getGennyToken().getToken());
-            long totalTime = TableUtils.searchTable(beUtils,searchBE, true);
-            System.out.println("total took " + (totalTime) + " ms");
-            /* Send out the Filter question group */
-//            TableUtils.sendFilterQuestions(beUtils, searchBE.getCode());
 
         }else{
             System.out.println("searchBE is null");
         }
 
+
+//  Create Remote Service Jobs
+        BaseEntity remoteJob1 = createRemoteJob("RJB_NLP_01","NLP Remote Job ADL","RMS_JNL_NLP_01");
+        BaseEntity remoteJob2 = createRemoteJob("RJB_REC_01","Rec Sys Host Companies","RMS_HC_RECSYS_01");
+        BaseEntity remoteJob3 = createRemoteJob("RJB_NLP_02","NLP Remote Job MEL","RMS_JNL_NLP_02");
+        BaseEntity remoteJob4 = createRemoteJob("RJB_REC_02","Rec Sys Intern Remote Job","RMS_INTERN_RECSYS_02");
+        BaseEntity remoteJob5 = createRemoteJob("RJB_CRED_01","Intern CRED Remote Job","RMS_INTERN_CRED_01");
+
+//  List Remote Service Jobs
+        //      Search Entity to be displayed as a table
+        SearchEntity searchRemoteJobs = new SearchEntity("SBE_REMOTE_JOB", "Remote AI Jobs")
+                .addSort("PRI_NAME","Name", SearchEntity.Sort.ASC)
+                .addFilter("PRI_CODE", SearchEntity.StringFilter.LIKE, "RJB_%")
+                .addColumn("PRI_NAME","Job Name")
+                .addColumn("PRI_STATUS","Status")
+                .addAssociatedColumn("LNK_REMOTE_SERVICE", "PRI_CODE", "RemoteServiceCode");
+        searchRemoteJobs.setRealm(realm);
+        searchRemoteJobs.setPageStart(0);
+        searchRemoteJobs.setPageSize(1000);
+
+        VertxUtils.putObject(beUtils.getGennyToken().getRealm(), "", searchRemoteJobs.getCode(), searchRemoteJobs,
+                beUtils.getGennyToken().getToken());
+//        beUtils.saveBaseEntity(searchRemoteJobs);
+
+        List<BaseEntity> remoteJobBES = beUtils.getBaseEntitys(searchRemoteJobs);
+
+// Send the display table data
         QCmdTableMessage msg = new QCmdTableMessage("SBE_TEST","RemoteServiceTest");
         JsonObject msgJson = new JsonObject();
         msgJson.put("cmd_type","DISPLAY");
@@ -256,19 +291,13 @@ public class RemoteServiceTest {
         msgJson.put("option","EXEC");
         msgJson.put("send",true);
         msgJson.put("token",beUtils.getGennyToken().getToken());
+
         VertxUtils.writeMsg("webcmds",msgJson.toString());
-
-
-
-
-
-//  Create Remote Service Jobs
-
-
-
-//  List Remote Service Jobs
-
-
+        long totalTime = TableUtils.searchTable(beUtils,searchBE, true);
+        System.out.println("total took " + (totalTime) + " ms");
+        /* Send out the Filter question group */
+        TableUtils.sendFilterQuestions(beUtils, searchBE.getCode());
+        VertxUtils.writeMsgEnd(beUtils.getGennyToken());
 
 
 //  Trigger one of the Remote Service Jobs
@@ -297,14 +326,35 @@ public class RemoteServiceTest {
     }
 
 
-    public BaseEntity createRemoteService(String code, String name, String url, String... searchBECodes) throws Exception {
+    public BaseEntity createRemoteService(String code, String name, String url, String authorCode,String... searchBECodes) throws Exception {
 
         // Use this bit to test creation of BEs by using their DEF_ names
         BaseEntity remoteServiceDef = beUtils.getDEFByCode("DEF_REMOTE_SERVICE");
         BaseEntity remoteServiceBE = beUtils.create(remoteServiceDef, name, code);
+        remoteServiceBE.setValue("PRI_NAME",name);
+        remoteServiceBE.setValue("PRI_URL",url);
+        remoteServiceBE.setValue("LNK_SEARCH_BES", "[\""+searchBECodes[0]+"\"]");
+        Attribute lnkAuthorAttr = RulesUtils.getAttribute("LNK_AUTHOR", beUtils.getGennyToken().getToken());
+        remoteServiceBE.addAnswer(new Answer(remoteServiceBE, remoteServiceBE, lnkAuthorAttr, "[\""+authorCode+"\"]"));
+//        remoteServiceBE.setValue("LNK_AUTHOR","[\""+authorCode+"\"]");
         remoteServiceBE.setStatus(EEntityStatus.ACTIVE);
         beUtils.saveBaseEntity(remoteServiceBE);
         return remoteServiceBE;
+    }
+
+    public BaseEntity createRemoteJob(String code, String name, String... remoteServiceCodes) throws Exception{
+
+        BaseEntity remoteJobDef = beUtils.getDEFByCode("DEF_REMOTE_JOB");
+        BaseEntity remoteJobBE = beUtils.create(remoteJobDef, name, code);
+        remoteJobBE.setValue("PRI_NAME", name);
+        Attribute lnkRemoteService = RulesUtils.getAttribute("LNK_REMOTE_SERVICE", beUtils.getGennyToken().getToken());
+        remoteJobBE.addAnswer(new Answer(remoteJobBE, remoteJobBE, lnkRemoteService, "[\""+remoteServiceCodes[0]+"\"]"));
+//        remoteJobBE.setValue("LNK_REMOTE_SERVICE", "[\""+remoteServiceCodes[0]+"\"]");
+        remoteJobBE.setValue("PRI_STATUS", "PROCESSING");
+        remoteJobBE.setStatus(EEntityStatus.ACTIVE);
+        beUtils.saveBaseEntity(remoteJobBE);
+        return remoteJobBE;
+
     }
 
     public void setUpDefs() throws BadDataException {
