44cd7eb826160662f17b9bd8212afbe8baf35a38
Jian Li <hbjoylee@gmail.com>
2021-11-23 01:51:39 +0000

    update check.sh , add checkrules version to match branch

--- check.sh
+++ check.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker run -v  "$PWD/rules:/rules"   gennyproject/checkrules  -a -r /rules
+branch=`git branch --show-current`
+echo "Current branch is ${branch}, will use checkrules:${branch}"
+docker pull gennyproject/checkrules:${branch}
+docker run -v "$PWD/rules:/rules"  gennyproject/checkrules:${branch}  -a -r /rules
 
